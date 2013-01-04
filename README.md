76f0c7f0f66e65763dec6a44c6386c8b7a7ee3e3
FG Ribreau <github@fgribreau.com>
2013-01-04 11:54:43 +0100

    Reorganizing folders

--- README.md
+++ README.md
@@ -1,5 +1,55 @@
-Demo and Documentation:
-http://sorich87.github.com/bootstrap-tour/
+# Bootstrap Tour
 
-TODO:
-Add "index" option to identify each step instead of using integers
+Quick and easy product tours with Twitter Bootstrap Popovers.
+
+Fork of the original [sorich87 bootstrap-tour](http://sorich87.github.com/bootstrap-tour/).
+
+## Getting Started
+Download the [production version][min] or the [development version][max].
+Or install with `npm install bootstrap-tour`
+
+[min]: https://raw.github.com/FGRibreau/bootstrap-tour/master/dist/bootstrap-tour.min.js
+[max]: https://raw.github.com/FGRibreau/bootstrap-tour/master/dist/bootstrap-tour.js
+
+In your web page:
+
+```html
+<script src="jquery.js"></script>
+<script src="jquery.cookie.js"></script>
+<script src="bootstrap.tooltip.js"></script>
+<script src="bootstrap.popover.js"></script>
+<script src="bootstrap-tour.min.js"></script>
+
+<script type="text/javascript">
+// Initialize the tour
+var tour = new Tour();
+
+//  Add steps
+tour.addStep({
+  element: "", /* html element next to which the step popover should be shown */
+  title: "", /* title of the popover */
+  content: "" /* content of the popover */
+});
+
+// Start the tour
+tour.start();
+</script>
+```
+
+## Documentation
+[Bootstrap-tour documentation](http://sorich87.github.com/bootstrap-tour/)
+
+## Release History
+v0.1.0 - Initial commit
+
+## License
+Copyright (c) 2013 FG Ribreau
+Licensed under the MIT, GPL licenses.
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [grunt](https://github.com/cowboy/grunt).
+
+### Important notes
+Please don't edit files in the `dist` subdirectory as they are generated via grunt. You'll find source code in the `src` subdirectory!
+
+While grunt can run the included unit tests via PhantomJS, this shouldn't be considered a substitute for the real thing. Please be sure to test the `test/*.html` unit test file(s) in _actual_ browsers.
---
+++ grunt.js
@@ -0,0 +1,85 @@
+/*global module:false*/
+module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-coffee');
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: '<json:package.json>',
+    meta: {
+      banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+        '<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
+        '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+        ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
+    },
+    concat: {
+      dist: {
+        src: ['<banner:meta.banner>', '<file_strip_banner:src/<%= pkg.name %>.js>'],
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+    min: {
+      dist: {
+        src: ['<banner:meta.banner>', '<config:concat.dist.dest>'],
+        dest: 'dist/<%= pkg.name %>.min.js'
+      }
+    },
+    qunit: {
+      files: ['test/**/*.html']
+    },
+    coffee: {
+      lib: {
+        src: 'src/*.coffee',
+        dest: 'lib/',
+        options: {
+            bare: true
+        }
+      },
+      test: {
+        src: 'test/*.coffee',
+        dest: 'test/',
+        options: {
+            bare: true
+        }
+      }
+    },
+    lint: {
+      files: ['grunt.js', 'src/**/*.js', 'test/**/*.js']
+    },
+    watch: {
+      files: ['src/*.coffee', 'test/*.coffee'],
+      tasks: 'coffee:lib coffee:test lint qunit'
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true,
+        browser: true
+      },
+      globals: {
+        Tour:true,
+        jQuery: true,
+        module:true,
+        $:true,
+        equal:true,
+        test:true,
+        ok:true,
+        deepEqual:true,
+        strictEqual:true
+      }
+    },
+    uglify: {}
+  });
+
+  // Default task.
+  grunt.registerTask('default', 'lint qunit concat min');
+
+};
