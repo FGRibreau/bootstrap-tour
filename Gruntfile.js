5a907aa5528434ba9b63bb3a742ab0c7d6502501
FG Ribreau <github@fgribreau.com>
2013-09-13 18:27:39 +0200

    updated docs

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,11 @@
 /*global module:false*/
 module.exports = function(grunt) {
-  grunt.loadNpmTasks('grunt-coffee');
+  // grunt.loadNpmTasks('grunt-coffee');
   grunt.loadNpmTasks('grunt-shell');
 
   // Project configuration.
   grunt.initConfig({
     pkg: '<json:package.json>',
-    meta: {
-      banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-        '<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
-        '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-        ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
-    },
     concat: {
       dist: {
         src: ['<banner:meta.banner>', 'lib/bootstrap-tour.js'],
